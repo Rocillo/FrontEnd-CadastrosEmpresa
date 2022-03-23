548f34891a935e736136a71279f72029420ec785
Rocillo <guilherme.rocillo@sp.senai.br>
2022-03-23 16:56:04 -0300

    att 23/03

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-function print(){
+function submit(){
+	var list = document.getElementById('users');
+	var username = document.getElementById('nomeDaPessoa').value;
+	var entrada = document.createElement('li');
+	var idade = document.getElementById('idadeDaPessoa').value;
+	entrada.appendChild(document.createTextNode(username + ', ' + idade + ' anos'));
 	var dateToday = new Date();
 	var dateTodayDia = ("0" + dateToday.getDate()).slice(-2);
 	var dateTodayMes = ("0" + (dateToday.getMonth() + 1)).slice(-2)
 	var dateTodayAno = dateToday.getFullYear();
 	var dateOne = document.getElementById("diaDoEvento");
 	var diaDeHoje = (dateTodayAno + '-' + dateTodayMes + '-' + dateTodayDia);
+	//	console.log(dateOne.value);
+	//	console.log(diaDeHoje);
 
-	console.log(dateOne.value);
-	console.log(diaDeHoje);
+		if (dateOne.value <= diaDeHoje) {    
+			alert("Data inválida!");    
+		}
 
-	if (dateOne.value < diaDeHoje) {    
-		alert("Não foi possível seguir com o processo de cadastro!");    
-	}
-	else {    
-		alert("A data escolhida é valida.");    
-	}
-} 
+		else if (username == "") {
+        	alert("Digite um nome!");
+        	return false;
+    	}
+
+		else if (idade < 18){
+			alert("Não é possível cadastrar pessoas com menos de 18 anos!");
+		}
+
+		else if (list.length > 10) {
+
+			alert("Capacidade máxima de participantes!")
+
+		}
+
+		else{
+			list.appendChild(entrada);
+		}
+	return false;
+}
+
