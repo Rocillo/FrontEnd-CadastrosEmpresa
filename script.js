ef9aa16d239d20471a20e1adddc1a29a87ef5142
Rocillo <guilherme.rocillo@sp.senai.br>
2022-03-24 08:25:46 -0300

    Atualização 24/03/22

--- script.js
+++ script.js
@@ -3,36 +3,29 @@ function submit(){
 	var username = document.getElementById('nomeDaPessoa').value;
 	var entrada = document.createElement('li');
 	var idade = document.getElementById('idadeDaPessoa').value;
-	entrada.appendChild(document.createTextNode(username + ', ' + idade + ' anos'));
+	var quantidade = list.childElementCount;
 	var dateToday = new Date();
 	var dateTodayDia = ("0" + dateToday.getDate()).slice(-2);
 	var dateTodayMes = ("0" + (dateToday.getMonth() + 1)).slice(-2)
 	var dateTodayAno = dateToday.getFullYear();
 	var dateOne = document.getElementById("diaDoEvento");
 	var diaDeHoje = (dateTodayAno + '-' + dateTodayMes + '-' + dateTodayDia);
-	//	console.log(dateOne.value);
-	//	console.log(diaDeHoje);
 
 		if (dateOne.value <= diaDeHoje) {    
-			alert("Data inválida!");    
+			window.alert("Data inválida!");
 		}
-
 		else if (username == "") {
         	alert("Digite um nome!");
         	return false;
     	}
-
 		else if (idade < 18){
 			alert("Não é possível cadastrar pessoas com menos de 18 anos!");
 		}
-
-		else if (list.length > 10) {
-
+		else if (quantidade >= 10) {
 			alert("Capacidade máxima de participantes!")
-
 		}
-
 		else{
+			entrada.appendChild(document.createTextNode(username + ', ' + idade + ' anos'));
 			list.appendChild(entrada);
 		}
 	return false;
